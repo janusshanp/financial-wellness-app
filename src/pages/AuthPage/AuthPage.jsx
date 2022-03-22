12cba4fdc034b5c4b92749c42b37f976f2481681
janusshan <janusshan.p@gmail.com>
2022-03-22 10:02:49 -0400

    saving for conclicts

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,12 @@
+import "./AuthPage.css"
+import {Link} from "react-router-dom";
+import {useRef, useState} from 'react'
+
+export default function AuthPage(props){
+
+    return(
+        <div className="authorization">
+            
+        </div>
+    )
+}
