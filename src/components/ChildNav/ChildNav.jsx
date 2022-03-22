6dd0a2e4c503c487fdcf8c854fa23295b0c12d62
Ryan Joseph <ryankjoseph@gmail.com>
2022-03-22 18:13:01 -0400

    Add route to nav and style child nav.

---
+++ src/components/ChildNav/ChildNav.jsx
@@ -0,0 +1,27 @@
+import './ChildNav.css'
+
+function ChildNav() {
+  return (
+    <div className="childNav">
+      <div className="nav-item" id="far-left-nav-img">
+        <img src ="far-left-nav-img.png"></img>
+      </div>
+      <div className="nav-item" id="center-left-nav-img">
+        <img src ="center-left-nav-img.png"></img>
+      </div>
+      <div className="nav-item" id="center-nav-img">
+        <img src ="center-nav-img.png"></img>
+      </div>
+      <div className="nav-item" id="center-right-nav-img">
+        <img src ="center-right-nav-img.png"></img>
+      </div>
+      <div className="nav-item" id="far-right-nav-img">
+        <img src ="far-right-nav-img.png"></img>
+      </div>
+
+
+      </div>
+  )
+}
+
+export default ChildNav
