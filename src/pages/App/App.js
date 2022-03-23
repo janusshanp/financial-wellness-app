87e762a194e7c558c60e961b96ebdf2f95f0e887
Ryan Joseph <ryankjoseph@gmail.com>
2022-03-23 10:23:19 -0400

    Send props to children in quiz page. Create data.

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -17,6 +17,25 @@ import ChildNav from '../../components/ChildNav/ChildNav';
 import Quiz from '../Quiz/Quiz';
 
 function App() {
+  const questions = [
+    {
+      question:"What should you do?",
+      answers:['Spend', 'Save', 'Budget', 'Invest'],
+      correctAnswer:'Invest',
+      explanation: "Investing is just the right thing to do"
+    },
+    {
+      question:"Who should you invest in?",
+      answers:['Tesla', 'GME', 'Yourself', 'This hackathon'],
+      correctAnswer:'Tesla',
+      explanation: 'Lowest rated EV, highest rated sigma male'
+    },
+    {
+      question:"What is antiwork?",
+      answers:['Not the move', 'Definitely the move', 'Move to a cheaper location', 'Move your body to the rythm'],
+      correctAnswer:'Invest',
+      explanation: 'just move your body'
+    }]
 
   const [loggedInUser, setLoggedInUser] = useState(null)
 
@@ -33,7 +52,7 @@ function App() {
           <Route path="/login" element={<Login setUserInState={setUserInState}/>}/>
 
           <Route path="/nav" element = {<ChildNav />} />
-          <Route path="/quiz" element = {<Quiz />} />
+          <Route path="/quiz" element = {<Quiz questions = {questions}/>} />
 
 
           <Route path="/child/signup" element={<ChildSignUp/>} />
