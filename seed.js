91370c141977e1857cd5540078ef8727433ef93f
Kevin <kevinpeterglen@gmail.com>
2022-03-23 12:57:31 -0400

    x

--- seed.js
+++ seed.js
@@ -8,9 +8,10 @@ const Topic = require('./models/topic');
 
 async function populateDB(){
     await Topic.deleteMany({})
+    let image = require('./src/images/spending_1.png')
     const newTopic = await Topic.create([
     {
-        imgUrl: '../../images/spending_1.png',
+        imgUrl: image,
         title: 'Spending',
         lesson: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco nisi'
     },
--- src/components/TopicSelector/TopicSelector.jsx
+++ src/components/TopicSelector/TopicSelector.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./TopicSelector.css";
 
+
 function TopicSelector(props) {
   return (
   <div className="topicSelector">
-      <img src={props.imgUrl}></img>
+      <img src={require(props.imgUrl)}></img>
+      
       {props.imgUrl}
       <p>{props.title}</p>
       
