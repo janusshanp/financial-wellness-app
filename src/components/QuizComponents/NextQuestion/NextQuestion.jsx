3b01c64d4a71968871213a61d8a9906c9774e62f
Ryan Joseph <ryankjoseph@gmail.com>
2022-03-23 22:50:15 -0400

    Next question will clear saved radio button states and go to next question in list.

--- src/components/QuizComponents/NextQuestion/NextQuestion.jsx
+++ src/components/QuizComponents/NextQuestion/NextQuestion.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react';
 import './NextQuestion.css'
 
-function NextQuestion() {
+function NextQuestion({questions, currentQuestion, setCurrentQuestion, options, setOptions, numberOfQuestions, checkOrNext, setCheckOrNext, nextOrContinue, setNextOrContinue,radioBtnValue, setRadioBtnValue, selectedRadioButtonEvent, setSelectedRadioButtonEvent, correctAnswerRef, explanationVisibility, setExplanationVisibility}) {
+  const [disabledState, setDisabledState] = useState(false)
+  useEffect(()=>{
+    console.log('questions NULLL', questions);
+  },[currentQuestion,disabledState]);
+
+  //next question button functionality
+ function next(){
+   //styling removed from components
+   setExplanationVisibility('hidden')
+   selectedRadioButtonEvent.checked=false;
+   selectedRadioButtonEvent.parentElement.style.border=""
+   selectedRadioButtonEvent.parentElement.style.opacity="1";
+   correctAnswerRef.current.style.border="";
+   if (nextOrContinue ==="Continue"){
+     console.log('CONTINUE TO LESSON PATH!')
+     return
+   }
+
+   if(currentQuestion+1 === numberOfQuestions){
+     console.log('disable button')
+     setDisabledState(true)
+     //potentially results page
+     setCheckOrNext('check')
+     return
+    }
+    setCurrentQuestion(currentQuestion+1)
+    setRadioBtnValue(null)
+    setCheckOrNext('check')
+  }
   return (
-    <div className="next-question">NextQuestion</div>
+    <div className="next-question">
+      <button disabled={disabledState} onClick={next} className="next-question-btn">{nextOrContinue}</button>	</div>
   )
 }
 
