ee5b891fa3e8e11c87bb240e47732c1be283aee8
janusshan <janusshan.p@gmail.com>
2022-03-24 22:22:26 -0400

    put back button

--- src/components/ChildDashboard/ChildDashboard.jsx
+++ src/components/ChildDashboard/ChildDashboard.jsx
@@ -14,12 +14,16 @@ export default function ChildDashboard(props){
     
 
     return(
-        <div className="child-dashboard">
+        <div>
             <ChildHeader/>
-            <h1 className="dash-title">Hello, {props.childUser.name}!</h1>
-            <div className="piggy-container">
-                <img src={require('../../images/piggy_child_dashboard.png')}></img>
-                <p>Balance: ${props.childUser.totalBalance}</p>
+            <div className="child-dashboard">
+                <h1 className="dash-title">Hello ! {props.childUser.name}</h1>
+                <div className="piggy-container">
+                    <img src={require('../../images/piggy_child_dashboard.png')}></img>
+                    <p>Balance: ${props.childUser.totalBalance}</p>
+                </div>
+                <button onClick={()=> navigate("/child/lesson")}className="dash-button">I want to collect coins!</button>
+                <ChildNav/>
             </div>
         </div>
     )
